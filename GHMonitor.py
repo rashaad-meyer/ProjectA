6f3399088e8dd0d3b09559d79f2ea50648af8cad
viibrem <rashaad.meyer@gmail.com>
2019-10-13 20:40:16 +0100

    Get RTC time working

--- GHMonitor.py
+++ GHMonitor.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 # Importing modules
 import blynklib
+import time
 import spidev # To communicate with SPI devices
 from numpy import interp	# To scale values
 from time import sleep	# To add delay
@@ -18,6 +19,12 @@ output_LDR = 0.0
 output_POT = 0.0
 output_TEMP = 0.0
 
+localtime=0
+systime=0
+date =0
+t0=time.time()
+sampletime=1
+int check = -1
 led_pin = 20
 i = 0
 GPIO.setmode(GPIO.BCM)
@@ -49,10 +56,16 @@ def read_virtual_pin_handler(pin):
   blynk.virtual_write(0, str(round(output_LDR,2)))
   blynk.virtual_write(1, str(round(output_POT,2)))
   blynk.virtual_write(2, str(round(output_TEMP,1)))
+  blynk.virtual_write(3, "Hello World\n")
 
 while True:
     blynk.run()
-    
+
+
+    localtime = time.strftime("%H:%M:%S", time.gmtime(time.time()))
+    systime = time.strftime("%H:%M:%S", time.gmtime(time.time()-t0))
+    second = int(localtime[-2:])
+
     output_LDR = analogInput(0) # Reading from CH0
     output_LDR = interp(output_LDR, [0, 1023], [0, 33])/10
     
@@ -62,8 +75,9 @@ while True:
     output_TS = analogInput(2) # Reading from CH2
     output_TS = interp(output_TS, [0, 1023], [0, 33])/10
     output_TEMP = Temp(output_TS)
-
-    
+    print(systime[-2:])
+    print(localtime)
+    print(systime)
     print("Light: " + str(round(output_LDR,2)))
     print("Humidity: " + str(round(output_POT,2)))
     print("Temp Voltage: " + str(output_TS))
