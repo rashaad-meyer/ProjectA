a5f06384441ef7e5c4b562e2971deba9c3fa4f27
viibrem <rashaad.meyer@gmail.com>
2019-10-14 00:02:44 +0100

    Added alarm and alarm reset

--- GHMonitor.py
+++ GHMonitor.py
@@ -7,6 +7,7 @@ from numpy import interp	# To scale values
 from time import sleep	# To add delay
 import RPi.GPIO as GPIO	# To use GPIO pins
 from prettytable import PrettyTable
+import os
 # Start SPI connection
 spi = spidev.SpiDev() # Created an object
 spi.open(0,0)	
@@ -21,6 +22,8 @@ output_POT = 0.0
 output_TEMP = 0.0
 output_DAC = 0
 alarm = 0
+alarmtime0 = 0
+alarmtime = 0
 
 localtime=0
 systime=0
@@ -32,8 +35,9 @@ check = -1
 send_blink = True
 
 monitoring = True
+cleartable = False
 
-t=PrettyTable(['RTC Time', 'Sys Timer', 'Humidity', 'Temp', 'Light', 'DAC Out', 'Alarm'])
+t=PrettyTable(['RTC Time', 'Sys Time', 'H', 'Temp', 'Light', 'DAC', 'Alarm'])
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(5, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
@@ -68,6 +72,26 @@ def toggleMonitoring(channel):
 
 GPIO.add_event_detect(6, GPIO.RISING, callback=toggleMonitoring, bouncetime=300)
 
+def resetCallback(channel):
+  
+  global cleartable
+  global t0
+
+  cleartable=True
+
+  t0=time.time()
+  
+
+GPIO.add_event_detect(13, GPIO.RISING, callback=resetCallback, bouncetime=300)
+
+def alarmResetCallback(channel):
+
+    global alarm
+    alarm = 0
+
+
+GPIO.add_event_detect(26, GPIO.RISING, callback=alarmResetCallback, bouncetime=300)
+
 # Read MCP3008 data
 def analogInput(channel):
     spi.max_speed_hz = 1350000
@@ -90,12 +114,16 @@ def Temp(data):
 def read_virtual_pin_handler(pin):
     
     global send_blink
+    global output_LDR
+    global output_POT
+    global output_TEMP
+    global systime
 
     if (send_blink and monitoring):
-        blynk.virtual_write(0, str(round(output_LDR,2)))
+        blynk.virtual_write(0, str(output_LDR))
         blynk.virtual_write(1, str(round(output_POT,2)))
         blynk.virtual_write(2, str(round(output_TEMP,1)))
-        blynk.virtual_write(3, t)
+        blynk.virtual_write(3, systime)
         send_blink = False
 
 
@@ -103,16 +131,26 @@ while True:
     
     blynk.run()
     
+    if cleartable:
+        t.clear_rows()
+        cleartable=False
+        os.system('clear')
+        send_blink=True
+
     localtime = time.strftime("%H:%M:%S", time.gmtime(time.time()))
     systime = time.strftime("%H:%M:%S", time.gmtime(time.time()-t0))
-    second = int(localtime[-2:])
+    alarmtime = time.strftime("%H:%M:%S", time.gmtime(time.time()-alarmtime0))
+    second = int(systime[-2:])
+    alarmtime_minute = int(alarmtime[3:5])
+    alarmtime_hour = int(alarmtime[:2])
+    alarmtime_second = int(alarmtime[-2:])
+
     if (second != check and second%sampletime==0 and monitoring):
         
-        
         check = second
-        print("Seconds: " + str(check))
+        print(str(alarmtime_hour) + str(alarmtime_minute) + str(alarmtime_second))
+        print( str(output_DAC) + "; Alarm:" + str(alarm))
         output_LDR = analogInput(0) # Reading from CH0
-        output_LDR = interp(output_LDR, [0, 1023], [0, 33])/10
     
         output_POT = analogInput(1) # Reading from CH1
         output_POT = interp(output_POT, [0, 1023], [0, 33])/10
@@ -121,10 +159,16 @@ while True:
         output_TS = interp(output_TS, [0, 1023], [0, 33])/10
         output_TEMP = Temp(output_TS)
 
+        output_DAC = (output_LDR/1023)*output_POT
+
+        if ((output_DAC>2.65 or output_DAC<0.65) and (alarmtime_minute>=3 or alarmtime_hour>=1)):
+            alarm = 1
+            alarmtime0 = time.time()
+
         send_blink = True
 
         t.add_row([str(localtime), str(systime), str(round(output_LDR,2)), str(round(output_POT,2)), str(round(output_TEMP,1)),str(round(output_DAC,2)), str(alarm)])
-        print(t)
+        #print(t)
 
         #print(systime[-2:])
         #print(localtime)
